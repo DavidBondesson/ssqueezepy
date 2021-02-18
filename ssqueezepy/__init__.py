d71493c3107ac261f1a411fb23d9660ea05fff14
David Bondesson <d919.bondesson@gmail.com>
2021-02-18 10:42:50 +0100

    accepted last changes to init.py in ssqueezep/ and name change back to ridge_extraction.py

--- ssqueezepy/__init__.py
+++ ssqueezepy/__init__.py
@@ -2,21 +2,16 @@
 """
 MIT License
 ===========
-
 Some ssqueezepy source files under other terms (see NOTICE.txt).
-
 Copyright (c) 2020 OverLordGoldDragon
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -42,14 +37,13 @@ from . import _ssq_stft
 from . import _gmw
 from . import _test_signals
 from . import wavelets
-from . import _extract_ridges
+from . import ridge_extraction
 from . import utils
 from . import toolkit
 from . import visuals
 from . import algos
 from . import configs
 from . import experimental
-
 
 
 from .ssqueezing import *
@@ -60,16 +54,8 @@ from ._ssq_stft import *
 from ._gmw import *
 from ._test_signals import *
 from .wavelets import *
-<<<<<<< HEAD
-<<<<<<< HEAD
-from ._extract_ridges import *
-=======
 from .ridge_extraction import *
->>>>>>> bc09289... added import to init.py and, test_signals and small change to ridge_extraction_test
-=======
-from ._extract_ridges import *
->>>>>>> a6dc52f... removed extra readme.md in ridge_extraction renamed ridge_extraction.py to _extract_ridges.py
-from ._test_signals import *
+
 
 def wavs():
     return wavelets.Wavelet.SUPPORTED
